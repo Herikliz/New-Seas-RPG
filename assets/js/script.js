14ab65e4bfe79579484a8a9b1e6bd096cccc5a5c
Herikliz <ericleseduardo353@gmail.com>
2026-06-29 12:56:44 -0300

    Site

--- assets/js/script.js
+++ assets/js/script.js
@@ -4090,6 +4090,7 @@ window.donosDeAkuma = {
     "Susu Susu no Mi": "Iori",
     "Toki Toki no Mi": "🔒FRUTA PERDIDA PELO MUNDO🔒",
     "Tori Tori no Mi, Modelo: Fênix": "Kozuki Shingen",
+    "Tori Tori no Mi, Modelo: Pássaro de Cinzas [Original do RPG]": "Genma Gan",
     "Tori Tori no Mi, Modelo: Thunderbird [Original do RPG]": "???",
     "Uo Uo no Mi, Modelo: Carpa Seiryū": "Keigo Kiyosaki",
     "Uta Uta no Mi": "???",
